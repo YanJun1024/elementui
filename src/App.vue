a62a36fe1a8b21fd7e558dbe6ad999dbb818947b
嘴角上扬 <18602495977@wo.cn>
2023-09-17 17:09:26 +0800

    已高亮

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <el-container style="height: 100vh; border: 1px solid #eee">
-    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu router :default-openeds="['1']">
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-tickets"></i>内容管理
+    <el-aside style="width:200px ;min-height:100vh; background-color:#001529;">
+      <div
+        style="height: 60px;background-color: #8c939d;color:white;display: flex;align-items: center;justify-content: center;">
+        logo
+      </div>
+      <el-menu style="border:none;" active-text-color="#fff" text-color="rgba(255,255,255,0.65)"
+        background-color="#001529" :default-activer="$route.path" router>
+        <el-menu-item index="/">
+          <template>
+            <i class="el-icon-house">
+              <span>系统首页</span>
+            </i>
           </template>
-          <el-menu-item-group>
-            <el-menu-item index="/articles/index">文章列表</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group>
-            <el-menu-item index="/articles/create">新建文章</el-menu-item>
-          </el-menu-item-group>
+        </el-menu-item>
+        <el-menu-item index="/element"> Element页面</el-menu-item>
+        <el-menu-item>
+          系统首页
+        </el-menu-item>
+        <el-menu-item>
+          系统首页
+        </el-menu-item>
+        <el-submenu>
+          <template slot="title">
+            <i class="el-icon-menu">
+              <span>系统首页</span>
+            </i>
+          </template>
+          <el-menu-item>
+            用户信息
+          </el-menu-item>
         </el-submenu>
       </el-menu>
     </el-aside>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,16 @@
 <template>
-  <div style="height:100vh">
-    <el-container style="height: 100vh; border: 1px solid #eee">
-
-      <el-container>
-        <el-header style="text-align: right; font-size: 12px">
-          <el-dropdown>
-            <i class="el-icon-setting" style="margin-right: 15px"></i>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>查看</el-dropdown-item>
-              <el-dropdown-item>新增</el-dropdown-item>
-              <el-dropdown-item>删除</el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-          <span>王小虎</span>
-        </el-header>
-
-        <el-main>
-          <el-table :data="tableData">
-            <el-table-column prop="date" label="日期" width="140">
-            </el-table-column>
-            <el-table-column prop="name" label="姓名" width="120">
-            </el-table-column>
-            <el-table-column prop="address" label="地址">
-            </el-table-column>
-          </el-table>
-        </el-main>
-      </el-container>
+  <div>
+    <el-container style="height: 100%; border: 1px solid #eee">
+      <el-main>
+        <el-table :data="tableData">
+          <el-table-column prop="date" label="日期" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="地址">
+          </el-table-column>
+        </el-table>
+      </el-main>
     </el-container>
 
   </div>
@@ -40,7 +25,7 @@ export default {
       address: '上海市普陀区金沙江路 1518 弄'
     };
     return {
-      tableData: Array(20).fill(item)
+      tableData: Array(10).fill(item)
     }
   },
 
